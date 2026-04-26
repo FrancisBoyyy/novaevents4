b0f318c9912a0323cd2dd580eaa086a9eb3c16b2
Francisco <fs.almeida@campus.fct.unl.pt>
2026-04-26 17:51:25 +0100

    first test (fazer figas malta) 2

--- src/test/kotlin/pt/unl/fct/iadi/novaevents/services/AppUserDetailsManagerTest.kt
+++ src/test/kotlin/pt/unl/fct/iadi/novaevents/services/AppUserDetailsManagerTest.kt
@@ -1,30 +1,33 @@
 package pt.unl.fct.iadi.novaevents.services
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
+
 import pt.unl.fct.iadi.novaevents.domain.AppUser
 import pt.unl.fct.iadi.novaevents.repository.AppUserRepository
 import pt.unl.fct.iadi.novaevents.security.AppUserDetailsManager
-import pt.unl.fct.iadi.novaevents.service.NovaEventsService
-import kotlin.test.Test
-import kotlin.test.assertEquals
 
 @ExtendWith(MockitoExtension::class)
-class NovaEventsServiceTest {
+class AppUserDetailsManagerTest {
+
     @Mock
     lateinit var repo: AppUserRepository
-    @InjectMocks
-    lateinit var service: NovaEventsService
+
     @InjectMocks
     lateinit var userService: AppUserDetailsManager
+
     @Test
-    fun `getByUsername returns user when found`() {
-        Mockito.`when`(repo.findByUsername("alice"))
+    fun getByUsernameReturnsUserWhenFound() {
+        `when`(repo.findByUsername("alice"))
             .thenReturn(AppUser(id = 1, username = "alice"))
-            val result = userService.loadUserByUsername("alice")
+
+        val result = userService.loadUserByUsername("alice")
+
         assertEquals("alice", result.username)
     }
 }
